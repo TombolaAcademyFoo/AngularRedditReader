cd5e72c7d595aed3183bdc3b72cee49262bc3715
keithbarrow <keith.barrow@TOMBOLA-0361.tombola.emea>
2015-05-15 16:44:46 +0100

    Added first factory test

--- main-app/tests/providers/model/about-factory.tests.js
+++ main-app/tests/providers/model/about-factory.tests.js
@@ -1,3 +1,33 @@
-/**
- * Created by keith.barrow on 15/05/2015.
- */
+(function () {
+    'use strict';
+    describe('about-factory tests',function(){
+        var aboutFactory;
+
+        beforeEach(module('Tombola.Reddit.Proxy'));
+
+        beforeEach(inject(function($injector){
+            aboutFactory = $injector.get('AboutFactory');
+        }));
+
+        it('about factory sets correctly', function(){
+            var fakeResponse = {
+                data:{
+                    display_name:'result-name',
+                    header_img: 'about-header-image',
+                    header_title: 'about-header-title',
+                    description_html: 'about-description',
+                    title: 'about-title',
+                    url: 'about-url'
+            }};
+
+            var result = aboutFactory(fakeResponse);
+            result.name.should.equal('result-name');
+            result.headerImage.should.equal('about-header-image');
+            result.headerTitle.should.equal('about-header-title');
+            result.descriptionHtml.should.equal('about-description');
+            result.title.should.equal('about-title');
+            result.url.should.equal('about-url');
+        });
+    });
+
+})();
